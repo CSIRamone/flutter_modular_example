c48767c832765a2499a4ed589de517abd70e9fc3
CSIRamone <cidinei_inacio@hotmail.com>
2025-05-12 22:39:38 -0300

    FlutterModular(ModularState e Disposable)

--- lib/app/categoria/model/categoria_controller.dart
+++ lib/app/categoria/model/categoria_controller.dart
@@ -1,9 +1,15 @@
 
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:modular_example_flutter/app/categoria/model/preco_model.dart';
 
-class CategoriaController {
+class CategoriaController extends Disposable {
   PrecoModel precoModel;
   CategoriaController({required this.precoModel}) {
     print('CategoriaController $hashCode');
+  }
+  
+  @override
+  void dispose() {
+  
   }
 }
